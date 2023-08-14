150fbbe19a832a69e01daa2634b127c95bafa934
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-08-14 06:10:33 +0100

    line spacing calrified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,41 @@
 import React from "react";
-import { FirstFooter, FooterContainer, SecondFooter } from "./footerStyled";
+import {
+  FooterContainer,
+  FirstFooter,
+  LeftFooter,
+  RightFooter,
+  SecondFooter,
+  Content,
+  TheNest,
+  Lounge,
+} from "./footerStyled";
+// import * as StyledComponents from './footerStyled';
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <FirstFooter></FirstFooter>
-      <SecondFooter></SecondFooter>
+      <FirstFooter>
+        <LeftFooter>
+          <Content>
+            <TheNest>The Nest</TheNest>
+            <Lounge>Lounge</Lounge>
+          </Content>
+        </LeftFooter>
+        <RightFooter>
+          <span>VISIT US</span>
+          <br></br>
+          <p>500 Terry Francine Street</p>
+          <p>San Francisco, CA 94158</p>
+          <p>Tel: 123-456-7890</p>
+          <p>​ Open from 6pm to 2am</p>
+          <p> WORK WITH US</p>
+          <p> join our team at the bar</p>
+          <p>Contact us at info@mysite.com</p>
+        </RightFooter>
+      </FirstFooter>
+      <SecondFooter>
+        &copy; 2023 By The Nest Lounge. Powered and secured by BBL
+      </SecondFooter>
     </FooterContainer>
   );
 };
--- src/components/Footer/footerStyled.jsx
+++ src/components/Footer/footerStyled.jsx
@@ -7,9 +7,61 @@ export const FooterContainer = styled.div`
 `;
 
 export const FirstFooter = styled.div`
-  height: 340px;
+  height: 345px;
+  display: flex;
+  flex-direction: row;
 `;
+
+export const LeftFooter = styled.div`
+  width: 59%;
+  margin: 0;
+  height: 100%;
+  line-height: 0.9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 150px;
+`;
+
+export const TheNest = styled.div`
+  font-size: 15px;
+  color: rgba(96 94 94);
+  padding: 0px;
+  margin: 0px;
+`;
+
+export const Lounge = styled.div`
+  font-size: 90px;
+  color: #000000b8;
+  padding: 0px;
+  margin: 0px;
+`;
+
+export const RightFooter = styled.div`
+  width: 41%;
+  margin: 0;
+  height: 100%;
+  line-height: 1.5em;
+  /* background-color: green; */
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 export const SecondFooter = styled.div`
   height: 40px;
   background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  color: #000000b8;
 `;
